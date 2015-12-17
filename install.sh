403c81a7c1e9d5566fe69ac5b958b88505fafdec
Erik Olson <xorgnak@gmail.com>
2015-12-17 11:21:45 -0700

    fixed script source.

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ echo 'su' > /usr/bin/leah
 chmod +x /usr/bin/leah
 apt-get install nmap arp-scan grep wpasupplicant macchanger tshark wifite netcat ii
 cp -fv leah.sh /root/leah.sh
-echo "source leah.sh" >> /root/.bashrc
+echo "source /root/leah.sh" >> /root/.bashrc
